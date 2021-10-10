7793bbc3735e3676dfe7c1155bcc6c8b6fc0345d
Virendra Kumar Patel <virendra.patel@news.co.uk>
2021-10-10 17:27:40 +0530

    changed App.js

--- Donewithit/App.js
+++ Donewithit/App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState();
+
+  useEffect(() => {
+    if (!counter) {
+      setInterval(() => {
+        setCounter(new Date().toLocaleTimeString());
+      }, 1000);
+    }
+  }, []);
+
+  const handleClick = (event) => {
+    console.log("Clicked....");
+    setCounter(counter + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Clicked {counter} Times</Text>
       <StatusBar style="auto" />
+
+      <Button
+        style={styles.button}
+        onPress={handleClick}
+        title="Increase Conuter"
+      ></Button>
     </View>
   );
 }
@@ -14,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
